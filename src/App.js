97179fd1e9b8b46d27ad1f5b34d6ab05e06cc832
Lukasinho <98915377+0xLukasinho@users.noreply.github.com>
2024-06-02 17:41:55 +0200

    Use HashRouter for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import GalleryPage from './pages/GalleryPage';
 import ComparePage from './pages/ComparePage';
-import SuperTrumpPage from './pages/SuperTrumpPage'; // Ensure this import is correct
+import SuperTrumpPage from './pages/SuperTrumpPage';
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/gallery" element={<GalleryPage />} />
           <Route path="/compare" element={<ComparePage />} />
-          <Route path="/super_trump" element={<SuperTrumpPage />} /> {/* Ensure this route is correct */}
+          <Route path="/super_trump" element={<SuperTrumpPage />} />
         </Routes>
       </main>
     </Router>
